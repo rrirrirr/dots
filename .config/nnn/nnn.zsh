224762522907f7c73944b42cfd62d92caa9f8871
rrirrirr <drpihl@gmail.com>
2022-11-30 00:25:08 +0100

    updates

---
+++ .config/nnn/nnn.zsh
@@ -0,0 +1,7 @@
+
+export NNN_PLUG='p:preview-tabbed;e:fzcd;a:fzopen;g:fzplug;m:mimelist;i:x2sel;j:nmount;o:imgview;d:copy-sel'
+# export NNN_FIFO=/tmp/nnn.fifo
+# BLK="f4" CHR="f4" DIR="e7" EXE="a0" REG="fc" HARDLINK="06" SYMLINK="06" MISSING="00" ORPHAN="09" FIFO="06" SOCK="0B" OTHER="06"
+# export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
+export NNN_TMPFILE='/tmp/.lastd'
+export NNN_FIFO=/tmp/nnn.fifo nnn
